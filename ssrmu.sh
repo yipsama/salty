ce480f523c7785e6a4656e1435051d3c8331a9e1
lemonsn <38351007+lemonsn@users.noreply.github.com>
2019-10-16 15:55:32 +0800

    fix bugs
    fix bugs

--- ssrmu.sh
+++ ssrmu.sh
@@ -5,12 +5,12 @@ export PATH
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
 #	Description: Install the ShadowsocksR mudbjson server
-#	Version: 1.0.6.3
+#	Version: 1.0.7
 #	Author: lemon
 #	Date: 2019/10/16
 #=================================================
 
-sh_ver="1.0.6.3"
+sh_ver="1.0.7"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
 ssr_folder="/usr/local/shadowsocksr"
